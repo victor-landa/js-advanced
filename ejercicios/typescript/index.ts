ef652e6ad967b3a232d62b5d3950858af4f4d5d5
Victor Landa <victorlanda.dev@gmail.com>
2020-01-22 17:25:44 -0600

    functions

--- ejercicios/typescript/index.ts
+++ ejercicios/typescript/index.ts
@@ -47,3 +47,36 @@ book = { type: "Notebook" };  // => ✅
 
 // -- -- -- Object -- -- -- //
 let someObject: object = { name: 'Poe' };
+
+
+
+// -- -- -- Functions -- -- -- //
+// Añadimos :number después de los parámetros para especificar
+// que tipo de dato va a regresar.
+function add(a: number, b: number): number {
+  return a + b;
+}
+
+const sum = add(1982, 1456);
+
+// -- -- ¿Qué pasa si una función regresa otra función? -- --
+// Después de los dos puntos representa la función, qué toma? un number y.. qué regresa? un number!
+function createAdder(a: number): (number) => number {
+  return function(b: number) {
+    return b + a;
+  }
+}
+
+const addFour = createAdder(4);
+const fourPlusSix = addFour(6);
+
+// -- -- -- Function with optional parameters -- -- --
+// Con ?: estamos permitiendo que el argumento que se reciba pueda ser undefined.
+// function showFullName(firstName: string, lastName?: string): string {
+// En este caso si no se provee un parámetro, la función tomará un argumento como placeholder.
+function showFullName(firstName: string, lastName: string = "Smith"): string {
+  return `${firstName} ${lastName}`;
+}
+
+const carl = showFullName("Carl");
+console.log(carl);
