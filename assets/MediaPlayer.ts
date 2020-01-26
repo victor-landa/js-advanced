cd0f48ea7a759c05e7183ad6526ef43aadba8480
Victor Landa <victorlanda.dev@gmail.com>
2020-01-25 18:59:06 -0600

    add ads... this is insane!

--- assets/MediaPlayer.ts
+++ assets/MediaPlayer.ts
@@ -2,8 +2,8 @@ class MediaPlayer {
   // Cuando tenemos la etqiueta video en HTML esa etiqueta representa
   // un tipo HTMLMediaElement.
   media: HTMLMediaElement;
-
   plugins: Array<any>;
+  container: HTMLElement;
 
   constructor(config) {
     this.media = config.el;
@@ -11,7 +11,16 @@ class MediaPlayer {
     this.plugins = config.plugins || [];
     // Antes escribiamos _initPlugin para comunicar que es privado, no se supone que
     // alguien lo llame directamente, excepto la misma clase.
+    this.initPlayer();
     this.initPlugins();
+  }
+
+  initPlayer() {
+    this.container = document.createElement('div');
+    this.container.style.position = 'relative';
+
+    this.media.parentNode.insertBefore(this.container, this.media);
+    this.container.appendChild(this.media);
   }
 
   private initPlugins() {
